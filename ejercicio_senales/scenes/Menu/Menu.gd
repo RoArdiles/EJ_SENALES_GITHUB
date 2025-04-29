e51a5ad62e620feb96ced0e06d65671fb2d7fc87
RoArdiles <ardilesrocio00@gmail.com>
2025-04-28 22:23:01 -0300

    crear_funcion_count_new_instance

--- ejercicio_senales/scenes/Menu/Menu.gd
+++ ejercicio_senales/scenes/Menu/Menu.gd
@@ -3,9 +3,14 @@ extends Node2D
 const INSTANCES_TEXT = "Cantidad de instancias: "
 # Variable para contar cantidad de instancias
 var instancesCount = 0
+@onready var count_label = $CountLabel
 
 func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
 	pass
+
+func count_new_instance():
+	instancesCount += 1
+	count_label.text = str(instancesCount)

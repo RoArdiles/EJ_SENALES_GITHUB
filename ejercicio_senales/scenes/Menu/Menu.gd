140c36dcf8827df156035540b64c792e22e2353f
RoArdiles <ardilesrocio00@gmail.com>
2025-04-28 22:57:45 -0300

    CONEXION_MENU_A_INSTANCIAS_FUNCION_COUNT_NEW_INSTANCE

--- ejercicio_senales/scenes/Menu/Menu.gd
+++ ejercicio_senales/scenes/Menu/Menu.gd
@@ -4,13 +4,22 @@ const INSTANCES_TEXT = "Cantidad de instancias: "
 # Variable para contar cantidad de instancias
 var instancesCount = 0
 @onready var count_label = $CountLabel
+@onready var red_generator = $RedGenerator
+@onready var violet_generator = $VioletGenerator
+
+func count_new_instance():
+	instancesCount += 1
+	count_label.text = str("Cantidad de instancias: ",instancesCount)
 
 func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
-	pass
+	red_generator.connect("button_down", Callable(self, "count_new_instance"))
+	violet_generator.connect("button_down", Callable(self, "count_new_instance"))
 
-func count_new_instance():
-	instancesCount += 1
-	count_label.text = str(instancesCount)
+
+
+#func count_new_instance():
+#	instancesCount += 1
+#	count_label.text = str(instancesCount)
